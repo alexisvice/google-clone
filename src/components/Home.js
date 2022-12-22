a3e203f1a2d44370708225c4ba7867f97bf02919
Alexis Vice <107651097+alexisvice@users.noreply.github.com>
2022-12-22 21:58:30 +0000

    week 3 search component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,25 +3,64 @@ import React from 'react'
 import AppsIcon from '@material-ui/icons/Apps';
 import AvatarIcon from '@material-ui/core/Avatar';
 
+import Search from './Search';
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`
+const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 30px;
+    align-items: center;
+`
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    p{
+        margin-right: 20px;
+        font-size: 15px;
+    }
+    .left-margin{
+        margin-left: 20px;
+    }
+`
+const BodyContainer = styled.div`
+    flex: 1;
+    display: flex;
+    margin-top: 10%;
+    flex-direction: column;
+    img{
+        onject-fit: contain;
+        height: 100px;
+}
+`
 const Home = () => {
     return (
-        <div className='home'>
-            <div className='header'>
-                <div className='header-left'>
+        <HomeContainer>
+            <HeaderContainer>
+                <Header>
                     <p>About</p>
                     <p>Store</p>
-                </div>
-                <div className='header-right'>
+                </Header>
+                <Header>
                     <p>Gmail</p>
                     <p>Images</p>
-                    <AppsIcon />
-                    <AvatarIcon />
-                </div>
-            </div>
-            <div className='body'>
+                    <AppsIcon className='left-margin'/>
+                    <AvatarIcon className='right-margin'/>
+                </Header>
+            </HeaderContainer>
+            <BodyContainer>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/320px-Google_2015_logo.svg.png" alt="google-logo" />    
-            </div>
-        </div>
+                <div>
+                    <Search />
+                </div>
+            </BodyContainer>
+        </HomeContainer>
+
     )
 }
 
